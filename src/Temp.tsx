5d156400707ec1462d445d04cb017ab04ac81247
Tatsu <ytatsu.sub@gmail.com>
2023-05-12 16:26:24 +0900

    ベジェ曲線描画

--- src/Temp.tsx
+++ src/Temp.tsx
@@ -1,25 +1,67 @@
 import React, {useState} from 'react';
 import './Temp.css';
 
+interface Circle {
+    id: number;
+    cx: number;
+    cy: number;
+    r: number;
+}
+
 function Temp() {
 
-    const [x1, setX1] = useState(100)
+    // 線をスライド
+    const [x1, setX1] = useState(100);
     const changeX1 = (x: string) => {
         console.log(x);
         setX1(Number(x));
+    };
+
+    // 円をクリックで変化
+    const [active, setActive] = useState(false);
+
+    // 2つの線を繋ぐベジェ曲線
+    const [circles, setCircles] = useState<Circle[]>([]);
+    const newCircle1: Circle = {
+        id: 1,
+        cx: 300,
+        cy: 300,
+        r: 50,
+    };
+    const newCircle2: Circle = {
+        id: 2,
+        cx: 500,
+        cy: 400,
+        r: 50,
+    };
+    const createCircle = () => setCircles([...circles, newCircle1, newCircle2]);
+    const [bezier, setBezier] = useState(``);
+    const calculateCurve = (circle1: Circle, circle2: Circle) => {
+        const spx = circle1.cx + circle1.r;
+        const spy = circle1.cy;
+        const epx = circle2.cx - circle2.r;
+        const epy = circle2.cy;
+        const shx = (spx + epx) / 2;
+        const shy = spy;
+        const ehx = (spx + epx) / 2;
+        const ehy = epy;
+        setBezier(`M${spx},${spy} C${shx},${shy} ${ehx},${ehy} ${epx},${epy}`);
     }
-
-    const [active, setActive] = useState(false)
-
 
     return(
         <div>
-            <svg width="300" height="300">
+            <svg width="800" height="500">
                 <line x1={x1} y1="100" x2="200" y2="200" stroke="black"></line>
                 <circle className={active ? "active" : ""} cx="150" cy="150" r="50" onClick={() => setActive(!active)}></circle>
+                {circles.map((c) => (
+                    <circle cx={c.cx} cy={c.cy} r={c.r}></circle>
+                ))}
+                <path d={bezier} stroke="black" stroke-width="2" fill='transparent'></path>
             </svg>
             <div>
                 <input type="range" value={x1} onChange={(e) => changeX1(e.target.value)}/>
+                <button onClick={createCircle}>円作成</button>
+                <button onClick={() => calculateCurve(circles[0], circles[1])}>繋ぐ</button>
             </div>
         </div>
     )
